3267f450706b7065f9ecda3b17d0d8e2e9c962ba
WaldoCuevas <104439067+WaldoCuevas@users.noreply.github.com>
2023-05-03 06:16:18 -0300

    Firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  //Metodos Get
+  urlGetData:'http://localhost:8080/api/get',
+  //Metodos Post
+  urlPostData:'http://localhost:8080/api/add',
+  //Metodos Put
+  urlPutData:'http://localhost:8080/api/modify',
+  //Metodos Delete
+  urlDeleteData:'http://localhost:8080/api/delete'
 };
